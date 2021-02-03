718c1625f631a5817c4899ab62379e3a36122939
Lactea98 <whdals7979@naver.com>
2021-02-03 17:46:06 +0900

    theme.js 버그 수정

--- router/theme.js
+++ router/theme.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const router = express.Router();
-const setTheme = require("../feature/themeMode");
 
 router.get("/theme", (req, res) => {
     const data = (req.query.data == "dark" ? "dark" : "light");
