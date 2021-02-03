21753345a0f5ebf9054dc05b2ad741e87bf630f8
Lactea98 <whdals7979@naver.com>
2021-02-03 17:43:54 +0900

    Setting dark mode using session

--- router/index.js
+++ router/index.js
@@ -3,11 +3,14 @@ const router = express.Router();
 
 router.get("/", (req, res) => {
     let user_theme = req.session.user_theme;
+    let check = true;
 
-    if(!user_theme)
+    if(!user_theme || user_theme == "dark"){
         user_theme = "dark";
+        check = false;
+    }
 
-    res.render("index", {web_theme : user_theme});
+    res.render("index", {web_theme : user_theme, check : check});
 })
 
 
