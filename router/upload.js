0817f21c1e91bb80041deb2e4db2c112f0c8e6e5
Lactea98 <whdals7979@naver.com>
2021-02-09 19:20:13 +0900

    파일 업로드 관련 기능 및 버그 수정.

--- router/upload.js
+++ router/upload.js
@@ -17,13 +17,37 @@ const upload = multer({
             cb(null, uuidv4() + path.extname(file.originalname));
         }
     }),
+    limits: {
+        fileSize: 5 * 1024 * 1024, // 5MB
+    },
+    fileFilter: function(_req, file, cb){
+        checkFileType(file, cb);
+    }
 }).single("file");
+
+function checkFileType(file, cb){
+    const filetypes = /pdf/;
+    const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
+    const mimetype = filetypes.test(file.mimetype);
+  
+    if(mimetype && extname){
+      return cb(null,true);
+    } else {
+      cb('pdf 파일만 허용 됩니다.');
+    }
+  }
+
 
 router.post("/upload", async (req, res) => {
     const data = await requirement.getRequireData(req.session);
     if(!(await checkOauth.checkAuth(req, res, data))) return;
-    
+
     upload(req, res, async (err) => {
+        if(err){
+            res.json({"result" : "error", "message" : err});
+            return;
+        }
+
         const file_path = "/uploads/" + req.file.filename;
         const user_id = req.session.passport.user.id;
         const row = await sendQuery(`SELECT user_id FROM tmp_post_attach WHERE user_id = ?`, [user_id]);
@@ -33,7 +57,7 @@ router.post("/upload", async (req, res) => {
         else
             await sendQuery(`UPDATE tmp_post_attach SET tmp_path = ? WHERE user_id = ?`, [file_path, user_id])
 
-        res.send("<script>alert('파일이 업로드 되었습니다.')</script>");
+        res.json({"result" : "success", "message" : "파일이 업로드 되었습니다.", "path" : file_path});
     })
 })
 
--- static/js/feature/uploadFile.js
+++ static/js/feature/uploadFile.js
@@ -1,6 +1,14 @@
+var pdf_total_page;
+
 function sendFileToServer(upload_file){
     fetch("/upload", {
         method : "POST",
         body : upload_file
+    })
+    .then(res => res.json())
+    .then(async (res) => {
+        alert(res.message);
+        pdf_total_page = await getTotalPage(res.path);
+        setTotalPage(pdf_total_page);
     })
 }
