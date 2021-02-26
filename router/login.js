d70af7f6bb2ad299910508b41a377e7932cf9428
Lactea98 <whdals7979@naver.com>
2021-02-27 00:06:56 +0900

    feed 기능 추가 완료. #27

--- router/login.js
+++ router/login.js
@@ -19,15 +19,21 @@ passport.use(new GoogleStrategy({
     }, function(accessToken, refreshToken, profile, done) {
         process.nextTick(async () => {
             user = profile;
-
-            const rows = await sendQuery(`SELECT user_id FROM user WHERE user_id = ?`, [profile.id]);
+            
+            const rows = await sendQuery(`SELECT user_id, user_email FROM user WHERE user_id = ?`, [profile.id]);
             if(rows.length == 0){
                 const user_id = profile.id;
                 const user_image = profile.photos[0].value;
                 const user_name = profile._json.name;
+                const user_email = profile.emails[0].value;
 
-                await sendQuery(`INSERT INTO user (user_id, user_name, user_image, auth, registration_date) VALUES
-                                               (?, ?, ?, "guest", sysdate())`, [user_id, user_name, user_image]);
+                await sendQuery(`INSERT INTO user (user_id, user_email, user_name, user_image, auth, registration_date) VALUES
+                                               (?, ?, ?, ?, "guest", sysdate())`, [user_id, user_email, user_name, user_image]);
+            }
+            else if(rows[0].user_email == null){
+                const user_id = profile.id;
+                const user_email = profile.emails[0].value;
+                await sendQuery (`UPDATE user SET user_email = ? WHERE user_id = ?`, [user_email, user_id]);
             }
             
             return done(null, user);
@@ -35,7 +41,7 @@ passport.use(new GoogleStrategy({
     }
 ));
 
-router.get('/login', passport.authenticate('google', { scope: ['profile']}));
+router.get('/login', passport.authenticate('google', { scope: ['profile', 'email']}));
 router.get("/logout", (req, res) => {
     req.session.passport = undefined;
     res.redirect("/");
