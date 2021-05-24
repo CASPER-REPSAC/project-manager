f243429b908ecb049d633649868a9c845d032128
Lactea98 <whdals7979@naver.com>
2021-05-24 11:05:43 +0900

    version 0.5

--- router/api/index.js
+++ router/api/index.js
@@ -3,8 +3,9 @@ const router = express.Router();
 const executeQuery = require("../../feature/db");
 
 router.get("/api/index", async (req, res) => {
-    const idx = Number(req.query.idx) ? 6 * (Number(req.query.idx) - 1): 1;
-    const posts_row = await executeQuery(`SELECT user_id, post_idx, writer, title, post_date, type, tag, thumbnail FROM post ORDER BY post_idx DESC LIMIT ${idx}, 6`);
+    const max_content_cnt = 9;
+    const idx = Number(req.query.idx) ? max_content_cnt * (Number(req.query.idx) - 1): 1;
+    const posts_row = await executeQuery(`SELECT user_id, post_idx, writer, title, post_date, type, tag, thumbnail FROM post ORDER BY post_idx DESC LIMIT ${idx}, max_content_cnt`);
     const promises = posts_row.map(async (row, idx) => {
         const user_image = await executeQuery(`SELECT user_image FROM user WHERE user_id = ?`, [row["user_id"]]);
         posts_row[idx]["user_image"] = user_image[0]["user_image"];
