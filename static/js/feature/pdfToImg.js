df0bcab93da962707491247751741ea2589ef992
Shin seong min <62112908+SmongsDev@users.noreply.github.com>
2025-02-07 16:09:27 +0900

    Front issue (#83)
    * 02/03 fix #77
    
    * 02/07 fix #66

--- static/js/feature/pdfToImg.js
+++ static/js/feature/pdfToImg.js
@@ -6,66 +6,59 @@ var _PDF_DOC,
 
 async function showPDF(pdf_url) {
     pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.10.38/pdf.worker.min.mjs';
+
     try {
         _PDF_DOC = await pdfjsLib.getDocument({ url: pdf_url }).promise;
-    }
-    catch(error) {
+    } catch (error) {
         alert(error.message);
     }
 
     _TOTAL_PAGES = _PDF_DOC.numPages;
-    const html = `<canvas id="pdf-canvas" class="test" width="400"></canvas>`;
     const section_count = $(".section-range").length;
 
-    for(let sec = 0; sec < section_count; sec++){
-        let range = $(".section-range")[sec].value.split("~").map((i) => Number(i));
+    for (let sec = 0; sec < section_count; sec++) {
+        let range = $(".section-range")[sec].value.split("~").map(Number);
 
-        for(let s = range[0]; s <= range[1]; s++){
-            $($(".project-section-image")[sec]).children().children().append(pdfToImgModalHTML(s,400));
-            showPage(s, $(".pdf-to-image-canvas")[s-1]);
-            showPage(s, $(".pdf-to-image-canvas2")[s-1]);
+        for (let s = range[0]; s <= range[1]; s++) {
+            let sectionElement = $(".project-section-image")[sec];
+            $(sectionElement).children().children().append(pdfToImgModalHTML(s, 400));
+
+            let previewCanvas = document.querySelector(`#pdf-canvas-${s}`);
+            showPage(s, previewCanvas);
         }
     }
 }
 
 // load and render specific page of the PDF
-async function showPage(page_no, _CANVAS) {
-    if(page_no > _TOTAL_PAGES || page_no < 1) {
+async function showPage(page_no, canvas) {
+    if (page_no > _TOTAL_PAGES || page_no < 1) {
         console.error(`Page number out of range ${page_no}/${_TOTAL_PAGES}`);
         return;
     }
-    _PAGE_RENDERING_IN_PROGRESS = 5;
-    _CURRENT_PAGE = page_no;
+
     try {
-        var page = await _PDF_DOC.getPage(page_no);
-    }
-    catch(error) {
-        console.log(error.message);
-    }
-    
-    const outputScale = window.devicePixelRatio || 1;
-    var pdf_original_width = page.getViewport({scale:1}).width;
-    var scale_required = (_CANVAS.width / outputScale) / pdf_original_width;
-    var viewport = page.getViewport({scale: scale_required * outputScale});
-    
-    _CANVAS.width = Math.floor(viewport.width * outputScale);
-    _CANVAS.height = Math.floor(viewport.height * outputScale);
-    _CANVAS.style.width = Math.floor(viewport.width) + 'px';
-    _CANVAS.style.height = Math.floor(viewport.height) + 'px';
-    
-    var render_context = {
-        canvasContext: _CANVAS.getContext('2d'),
-        viewport: viewport,
-        transform: [outputScale, 0, 0, outputScale, 0, 0]
-    };
-    
-    try {
+        let page = await _PDF_DOC.getPage(page_no);
+
+        const outputScale = window.devicePixelRatio || 1;
+        let viewport = page.getViewport({ scale: canvas.width / page.getViewport({ scale: 1 }).width * outputScale });
+
+        canvas.width = Math.floor(viewport.width * outputScale);
+        canvas.height = Math.floor(viewport.height * outputScale);
+        canvas.style.width = Math.floor(viewport.width) + 'px';
+        canvas.style.height = Math.floor(viewport.height) + 'px';
+
+        let render_context = {
+            canvasContext: canvas.getContext('2d'),
+            viewport: viewport,
+            transform: [outputScale, 0, 0, outputScale, 0, 0]
+        };
+
         await page.render(render_context);
-    }
-    catch(error) {
+    } catch (error) {
         console.log(error.message);
     }
 }
+        
 
 
 async function getTotalPage(pdf_url){
--- static/js/previewPDF.js
+++ static/js/previewPDF.js
@@ -1,18 +1,22 @@
 const initPDF = async (path) => {
     pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.10.38/pdf.worker.min.mjs';
+
     try {
-        _PDF_DOC = await pdfjsLib.getDocument({url: path}).promise;
-    }
-    catch(error) {
+        _PDF_DOC = await pdfjsLib.getDocument({ url: path }).promise;
+    } catch (error) {
         alert(error.message);
     }
 
     _TOTAL_PAGES = _PDF_DOC.numPages;
-    for(let i=1; i<_TOTAL_PAGES+1; i++){
+
+    for (let i = 1; i <= _TOTAL_PAGES; i++) {
         $(".pdf-preview").append(pdfPreviewHTML(225, i));
-        showPage(i, $(".pdf-to-image-canvas")[i-1]);
+
+        let previewCanvas = document.querySelector(`#pdf-canvas-${i}`);
+        showPage(i, previewCanvas);
     }
+
     setTimeout(() => {
-        $($(".pdf-to-image-canvas")[0]).css("display", "");
-    },1000);
-}
+        document.querySelector(`#pdf-canvas-1`).style.display = "";
+    }, 1000);
+};
